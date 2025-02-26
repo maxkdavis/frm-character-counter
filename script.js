4a41e2331a14ba59f0dda2cf41c4aec49d8d67e7
maxkdavis <7098417+maxkdavis@users.noreply.github.com>
2025-02-25 21:53:27 -0500

    word count functionality

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const userInputEl = document.getElementById('user-input');
 const spacesCheckbox = document.getElementById('checkbox-space');
+const noSpaceTextEl = document.querySelector('.no-space-text');
 let characterCounter = 0;
+let wordCounter = 0;
+
+spacesCheckbox.addEventListener('change', function () {
+  noSpaceTextEl.classList.toggle('hidden');
+});
 
 userInputEl.addEventListener('input', function (e) {
   displayCharCount(e.target.value);
+  displayWordCount(e.target.value);
 });
 
 function displayCharCount(str) {
@@ -20,6 +27,12 @@ function displayCharCount(str) {
   }
 }
 
-spacesCheckbox.addEventListener('change', function () {
-  document.querySelector('.excludedText').classList.toggle('excludedText');
-});
+function displayWordCount(str) {
+  const trimmedStr = str.trim(); //remove leading and trailing spaces
+  const words = trimmedStr.split(' ').filter((word) => word !== ' ');
+  wordCounter = words.length;
+  formattedWordStr = wordCounter.toString().padStart(2, '0');
+  document.querySelector('.word-count').innerText = formattedWordStr;
+}
+
+//why does word count show 1 after i deleted everything??
